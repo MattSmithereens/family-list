5b38767ca2f240e3ec0a8781eb73a2a4001f92b4
Annie Shin and Matt Smith <matt@>
2018-06-04 11:14:53 -0700

    add loop for userinput

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    var userInput = "";
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
     blanks.forEach(function(blank) {
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
+      userInput = $("input#" + blank).val();
+      $("#thisIsMyList").append("<li>" + userInput + "</li>");
+      console.log(userInput);
     });
-    // var person1Input = $("input#person1").val();
-    // var person2Input = $("input#person2").val();
-    // var animalInput = $("input#animal").val();
-    // var exclamationInput = $("input#exclamation").val();
-    // var verbInput = $("input#verb").val();
-    // var nounInput = $("input#noun").val();
-    //
-    // $(".person1").text(person1Input);
-    // $(".person2").text(person2Input);
-    // $(".animal").text(animalInput)
-    // $(".exclamation").text(exclamationInput)
-    // $(".verb").text(verbInput)
-    // $(".noun").text(nounInput)
+
+
+    // $("#formOne").click(function() {
+    //   $("ul#formOne").append("<li>")
+    // }
 
     $("#story").show();
 
